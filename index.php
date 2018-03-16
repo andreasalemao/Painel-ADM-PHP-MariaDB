5e861299517e19f6ceac7440cddb15c137d4ed59
Andreas Siebert <andreassiebert3@gmail.com>
2018-03-16 11:27:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="author" content="Andreas Siebert">
+		
+		<title>Painel-ADM</title>
+		
+		<!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+
+	</head>
+	
+
+<body>
+
+	<div class="container-fluid">
+	
+		Pagina Administtrativa.
+
+	</div>
+
+</body>	
